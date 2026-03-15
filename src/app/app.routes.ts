a1930da4d4b65661ef9ee252a5d15f0de2266a5c
Alibakkali10 <148981870+Alibakkali10@users.noreply.github.com>
2026-03-14 21:38:20 -0400

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Register } from './register/register';
+import { Profile } from './profile/profile';
+import { authGuard } from './guards/authguards';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: 'profile', component: Profile, canActivate: [authGuard] },
+  { path: '**', redirectTo: 'login' }
+];
